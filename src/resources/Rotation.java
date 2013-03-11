a45cba2078a102fbc0e49762d9fdfa18df549f27
Leo D. Rausch <leo.d.rausch@gmail.com>
2013-03-11 17:08:39 -0400

    Documented Rotation.java

--- src/resources/Rotation.java
+++ src/resources/Rotation.java
@@ -3,8 +3,21 @@ package resources;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The following class rotates numbers.
+ * 
+ * For instance if you had the number 197, the rotations would be 719 and 971 and 197.
+ * 
+ * @author Leo Rausch
+ *
+ */
 public class Rotation {
 
+	/**
+	 * This method rotates a number for you. For instance if you had the number 197, the rotations would be 719 and 971 and 197.
+	 * @param n The starting number.
+	 * @return A list of possible rotations of the given number. 
+	 */
 	public static List<Integer> rotations(Integer n) {
 		List<Integer> rotationList = new ArrayList<Integer>();
 
